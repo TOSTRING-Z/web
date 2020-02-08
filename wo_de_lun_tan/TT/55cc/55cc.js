9de7f839c808fba6d977195ced343b7acdeb6281
TOSTRING-Z <1084337496@qq.com>
2020-02-08 18:19:22 +0800

    remove sql **

--- wo_de_lun_tan/TT/55cc/55cc.js
+++
@@ -1,8 +0,0 @@
-id = '';
-$('.ff-vod-img-new > a').attr('href', function (i, d) {
-    if(i==0)
-        id += d.split('/')[2];
-    else
-        id += (',' + d.split('/')[2]);
-});
-id = '['+id+']';
